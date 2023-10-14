b3c82fce432519dc15f66b8395aca0a6e6a5b190
Mukhammad <mukhammaddev9@gmail.com>
2023-10-14 17:26:14 +0500

    Added Footer

--- src/components/Mobile/index.jsx
+++ src/components/Mobile/index.jsx
@@ -4,11 +4,16 @@ const Mobile = ({ children }) => {
 	return (
 		<div className="mobile">
 			<header>
-				<img src="/img/header.webp" alt="Restaurant"/>
+				<img src="/img/header.webp" alt="Restaurant" />
 			</header>
 			<main>
 				{ children }
 			</main>
+			<footer>
+				<a href="https://github.com/themaverickmu" target="_blank" title="Website Developer">
+					Developed by: themaverickmu
+				</a>
+			</footer>
 		</div>
 	);
 }
