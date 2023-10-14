68d93c164407f9eacdadcc4872b53fe9b0f0e9e0
Mukhammad <mukhammaddev9@gmail.com>
2023-10-14 16:32:31 +0500

    Added header image

--- src/components/Mobile/index.jsx
+++ src/components/Mobile/index.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const Mobile = ({ children }) => {
-	return <div className="mobile">{ children }</div>
+	return (
+		<div className="mobile">
+			<header>
+				<img src="/img/header.webp" alt="Restaurant"/>
+			</header>
+			<main>
+				{ children }
+			</main>
+		</div>
+	);
 }
 
 export default Mobile;
